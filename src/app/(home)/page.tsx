f003acf3dc5a0a7d67e1868d0152a6b712a416b7
IlMelons <melonss2002@gmail.com>
2025-06-10 21:31:42 +0200

    Removed Unused import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useRef, useEffect, useState } from 'react';
 import { FaGithub, FaDiscord, FaUsers, FaBullseye, FaCogs, FaStar, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
