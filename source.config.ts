9a4accadff8233d418a652bab84f04f366fe0b61
IlMelons <melonss2002@gmail.com>
2025-06-16 19:24:15 +0200

    Test

--- source.config.ts
+++ source.config.ts
@@ -18,6 +18,6 @@ export const docs = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    providerImportSource: '@/mdx-components',
   },
 });
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -10,3 +10,5 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     GithubInfo,
   };
 }
+
+export const useMDXComponents = getMDXComponents;
