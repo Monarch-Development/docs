1bf69b3dbeca7e041ac8ea341dedc0e2939147ad
IlMelons <melonss2002@gmail.com>
2025-06-16 19:19:43 +0200

    .

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,14 +1,12 @@
-import defaultComponents from 'fumadocs-ui/mdx';
+import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { GithubInfo } from 'fumadocs-ui/components/github-info';
-import { TypeTable } from 'fumadocs-ui/components/type-table';
 
 // MDXComponents
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultComponents,
+    ...defaultMdxComponents,
     ...components,
     GithubInfo,
-    TypeTable,
   };
 }
