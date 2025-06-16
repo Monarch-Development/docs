74ec59d11c2ec45853ee9daeea5d948dcf377264
IlMelons <melonss2002@gmail.com>
2025-06-16 19:12:45 +0200

    fix components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,4 +1,4 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
+import defaultComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { GithubInfo } from 'fumadocs-ui/components/github-info';
 import { TypeTable } from 'fumadocs-ui/components/type-table';
